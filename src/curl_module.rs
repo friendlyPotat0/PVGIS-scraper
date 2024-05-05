3fa6e4af4d0bead26cea3f394af9f379352e9206
friendlyPotat0 <friendlyPotat0@protonmail.com>
2024-05-05 14:14:44 -0500

    Progress status added

--- src/curl_module.rs
+++ src/curl_module.rs
@@ -1,6 +1,7 @@
 use curl::easy::Easy;
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
 
 pub struct CurlModule {
     easy: Easy,
@@ -15,11 +16,20 @@ impl CurlModule {
         let mut file = File::create(filename).expect("Failed to create file");
 
         self.easy.url(url).expect("Failed to set URL");
-        
+
         self.easy.write_function(move |data| {
             file.write_all(data).expect("Failed to write data to file");
             Ok(data.len())
         }).expect("Failed to set write function");
+
+        let filename = Path::new(filename).file_name().unwrap().to_str().unwrap().to_owned();
+        self.easy.progress(true).unwrap();
+        self.easy.progress_function(move |_dltotal, dlnow, _ultotal, _ulnow| {
+            print!("\r{}: {} received bytes ", filename, dlnow);
+            std::io::stdout().flush().unwrap();
+            true
+        }).unwrap();
+        println!("");
 
         self.easy.perform().expect("Failed to perform download");
     }
