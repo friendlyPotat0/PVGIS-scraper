efd5c89b89074f5f0389e07182d8aaa6cc7f69f4
friendlyPotat0 <friendlyPotat0@protonmail.com>
2024-05-08 11:02:06 -0500

    Variable path for saving scraped files

--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,36 @@ fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     let mut geographic_bitmap_analysis = GeographicBitmapAnalysis::new("resources/map.png").expect("Failed to create GeographicBitmapAnalysis");
     let mut curl_module = CurlModule::new();
-    let n: i32 = request_input(&String::from("Enter number of times to scrape: "));
+    let n: i32 = request_num_input(&String::from("Enter number of times to scrape: "));
     for _ in 0..n {
         let (latitude, longitude) = geographic_bitmap_analysis.get_random_coordinate_on_land();
         let url = format!("https://re.jrc.ec.europa.eu/api/v5_2/seriescalc?lat={:.3}&lon={:.3}&browser=0&outputformat=json&optimalangles=1", latitude, longitude);
-        let filename = format!("resources/solar-radiation-database/timeseries_{:.3}_{:.3}.json", latitude, longitude);
+        let path = request_string_input(&String::from("Enter path to store scraped files: "));
+        let path = path.strip_suffix('/').unwrap_or(&path);
+        let filename = format!("{}/timeseries_{:.3}_{:.3}.json", path, latitude, longitude);
+        println!("{filename}");
         curl_module.download(&url, &filename);
     }
 }
 
-fn request_input(message: &String) -> i32 {
+fn request_num_input(message: &String) -> i32 {
     let mut num = String::new();
     print!("{}", message);
     io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout");
     io::stdin()
         .read_line(&mut num)
         .expect("Failed to read line");
-    let num: i32 = match num.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            return -1;
-        }
-    };
+    let num: i32 = num.trim().parse().expect("Input must be an integer");
     return num;
+}
+
+fn request_string_input(message: &String) -> String {
+    let mut string = String::new();
+    print!("{}", message);
+    io::Write::flush(&mut io::stdout()).expect("Failed to flush stdout");
+    io::stdin()
+        .read_line(&mut string)
+        .expect("Failed to read line");
+    let string = string.trim();
+    return string.to_string();
 }
